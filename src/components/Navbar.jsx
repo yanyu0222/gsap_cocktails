30c09e6b561655de43a2cd71ab787664aef50a10
Yanyu <jkf95338@gmail.com>
2025-06-26 19:01:59 +0800

    lead視差效果完成 1:11:19

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import gsap from "gsap";
+import { navLinks } from "../../constants";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
+    });
 
-export default Navbar
+    navTween.fromTo(
+      "nav",
+      {
+        backgroundColor: "transparent",
+      },
+      {
+        backgroundColor: "#00000050",
+        backgroundFilter: "blur(10px)",
+        ease: "power1.inOut",
+        duration: 1,
+      }
+    );
+  });
+
+  return (
+    <nav>
+      <div>
+        <a href="#home" className="flex items-center gap-2">
+          <img src="/images/logo.png" alt="logo" />
+          <p>Velvet Pour</p>
+        </a>
+
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
