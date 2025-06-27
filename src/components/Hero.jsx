54d28e45413e5eaac6e5550ac4db16fb71f98434
Yanyu <jkf95338@gmail.com>
2025-06-27 19:07:32 +0800

    Implement Cocktails Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,14 @@
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
 import gsap from "gsap";
-import React from "react";
+import React, { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef();
+
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   useGSAP(() => {
     const heroSplit = new SplitText(".title", { type: "chars, words" });
     const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
@@ -26,18 +31,37 @@ const Hero = () => {
       delay: 1,
     });
 
-    gsap.timeline({
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
+      .to(".right-leaf", { y: 200 }, 0)
+      .to(".left-leaf", { y: -200 }, 0);
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    // video animation timeline
+    const tl = gsap.timeline({
       scrollTrigger: {
-        trigger: "#hero",
-        start: "top top",
-        end: "bottom top",
+        trigger: "video",
+        start: startValue,
+        end: endValue,
         scrub: true,
+        pin: true,
       },
-    })
-    .to('.right-leaf', {y:200},0)
-    .to('.left-leaf', {y:-200}, 0)
-    
-    ;
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
   });
 
   return (
@@ -76,6 +100,16 @@ const Hero = () => {
           </div>
         </div>
       </section>
+
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+        ></video>
+      </div>
     </>
   );
 };
