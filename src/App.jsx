14f9e0c3fbca444b381969e57142f5028dba8844
Yanyu <jkf95338@gmail.com>
2025-07-03 12:46:03 +0800

    修正大小寫

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import Cocktails from "./components/cocktails";
+import Cocktails from "./components/Cocktails.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
